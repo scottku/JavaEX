7cd8c3c2f6cba729eb0bba647d2f85e68096560e
구본재 <scottku@naver.com>
2021-05-03 14:19:24 +0900

    콘솔 입력

--- JavaEx/src/com/javaex/basics/ConsoleEx.java
+++ JavaEx/src/com/javaex/basics/ConsoleEx.java
@@ -1,11 +1,30 @@
 package com.javaex.basics;
-
+import java.util.Scanner; //java.util 패키지의 Scanner 클래스를 로드하는 기능
 public class ConsoleEx {
 
 	public static void main(String[] args) {
 	
-		consoleOutput();
+		//consoleOutput();
+		consoleInput();
+	}
+	
+	private static void consoleInput() {
+		// Class를 불러올 때 ctrl + shift + o 로 확인
+		// 필요한 클래스 들어있는 패키지를 정확히 지정할 것
+		// 자동 완성 기능 활용 : ctrl + space
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("이름을 입력해 주세요.");
+		System.out.println("이름 : ");
 		
+		String name = scanner.next();
+		
+		System.out.println("나이를 입력해 주세요.\n나이 : ");
+		
+		int age = scanner.nextInt();
+		
+		System.out.println("당신의 이름은 " + name + " 이고 나이는 " + age + " 세 입니다.");
+		
+		scanner.close();
 	}
 	private static void consoleOutput() {
 		// print -> 출력 후 개행 안함
