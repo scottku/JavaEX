1b606e026be44adb0b8ae3ebde46775a07879e08
scott <scott@192.168.219.106>
2021-05-07 16:30:51 +0900

    얕은, 깊은 복제

---
+++ JavaEx/src/com/javaex/api/objectclass/v4/LangClassTest.java
@@ -0,0 +1,26 @@
+package com.javaex.api.objectclass.v4;
+
+public class LangClassTest {
+
+	public static void main(String[] args) {
+		Scoreboard s1 = new Scoreboard(new int[] {10, 20, 30, 40});
+		System.out.println("s1 = " + s1);
+		
+		// 참조 복제 : 같은 객체
+		Scoreboard s2 = s1;
+		System.out.println("s1 == s2? : " + (s1==s2));
+		
+		// 얕은 복제
+		s2 = s1.getClone();
+		System.out.println("s2 = " + s2);
+		
+		// 확인
+		System.out.println("s1 == s2? : " + (s1==s2));
+		
+		System.out.println("s1.scores == s2.scores ? :" + (s1.getScores() == s2.getScores()));
+		s2.getScores()[2] = 100;
+		System.out.println("s1 = " + s1);
+		System.out.println("s2 = " + s2); //clone 메서드 override 안하면 s2를 바꿨음에도 s1의 데이터가 변경됨
+	}
+
+}
